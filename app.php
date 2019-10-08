a11c51b1c6eeb2b72eb2b53709959bf4fd52f973
Rahimie Ahmad <newrehmi@gmail.com>
2019-10-08 16:46:34 +0800

    Update app.php

--- app.php
+++ app.php
@@ -8,10 +8,10 @@ $app = new \App\App(__DIR__, \Exedra\Runtime\Context::class);
 | Providers Registry
 |--------------------------------------------------------------------------
 */
+$app->provider->add(new \Exedra\Routeller\RoutellerRootProvider(\App\Controllers\RootController::class));
 $app->provider->add(\App\Providers\KernelProvider::class);
 $app->provider->add(\App\Providers\DotEnvProvider::class);
 $app->provider->add(\Exedra\Console\ConsoleProvider::class);
-$app->provider->add(new \Exedra\Routeller\RoutellerRootProvider(\App\Controllers\RootController::class));
 $app->provider->add(\App\Providers\TwigProvider::class);
 $app->provider->add(\App\Providers\EloquentProvider::class);
 
